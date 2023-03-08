5eaa29b1d19c485b74d0e4e58c03307507b8db27
Mathieu BRUN <>
2023-03-08 21:20:35 +0100

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from lark import Lark
+
+l = Lark('''start: WORD "," WORD "!"
+
+            %import common.WORD   // imports from terminal library
+            %ignore " "           // Disregard spaces in text
+         ''')
+
+print( l.parse("Hello, World!") )
