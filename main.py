a746db8cbaaf5edff3e1c3f0e4ae42dfa711fa0f
Mathieu BRUN <>
2023-03-08 21:47:27 +0100

    feat: multiline json

--- main.py
+++ main.py
@@ -2,10 +2,37 @@
 
 from lark import Lark
 
-l = Lark('''start: WORD "," WORD "!"
+l = Lark(r"""
+    ?value: dict
+          | list
+          | string
+          | SIGNED_NUMBER      -> number
+          | "true"             -> true
+          | "false"            -> false
+          | "null"             -> null
 
-            %import common.WORD   // imports from terminal library
-            %ignore " "           // Disregard spaces in text
-         ''')
+    list : "[" [value ("," value)*] "]"
 
-print( l.parse("Hello, World!") )
+    dict : "{" [pair ("," pair)*] "}"
+    pair : string ":" value
+
+    string : ESCAPED_STRING
+
+    %import common.ESCAPED_STRING
+    %import common.SIGNED_NUMBER
+    %import common.WS
+    %ignore WS
+
+    """, start='value')
+
+text = '''{"key": [
+   "item0", 
+   "item1", 
+   3.14, 
+   true
+   ]
+}'''
+
+print(text)
+print("".ljust(10, "="))
+print(l.parse(text).pretty())
